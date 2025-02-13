3aa08d9d1f3403123a6d0bba723a021a6b42069a
emily202777 <126031180+emily202777@users.noreply.github.com>
2025-02-12 19:32:17 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# carbonOutputMetric = string containing undeclared variable carbon output
+# loggingStreak = string containing undeclared variable streak days of logging
+# dietScore = string containing undeclared variable sustainability of diet score
+
+#carbon output to be measured in kilograms of carbon dioxide
